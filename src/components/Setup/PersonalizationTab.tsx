a6318a66ce64bdd1af28be20389ecdc2c5c407e0
DeprecatedLuxas <lucasmathias65@icloud.com>
2021-12-06 16:34:08 +0100

    personalization tab changes

--- src/components/Setup/PersonalizationTab.tsx
+++ src/components/Setup/PersonalizationTab.tsx
@@ -2,7 +2,25 @@ import { useRecoilState } from "recoil";
 import { setupState } from "@recoil/atoms/setup";
 import Select, { ActionMeta, SingleValue } from "react-select";
 import { useCallback } from "react";
-import { SideBarPlacement } from "@lib/types";
+import { IconPack, SideBarPlacement } from "@lib/types";
+
+const iconPacks: {
+  name: string;
+  value: IconPack;
+}[] = [
+  {
+    name: "Seti Icons",
+    value: "seti-icons",
+  },
+  {
+    name: "Material Icons",
+    value: "material-icons",
+  },
+  {
+    name: "VSCode Icons",
+    value: "vscode-icons",
+  },
+];
 
 export default function PersonalizationTab() {
   const [config, setConfig] = useRecoilState(setupState);
@@ -18,12 +36,33 @@ export default function PersonalizationTab() {
         label: string;
       }>
     ) => {
-      console.log(option, actionMeta);
       setConfig({
         ...config,
         options: {
           ...config.options,
           sidebar: option?.value || "left",
+        },
+      });
+    },
+    [config, setConfig]
+  );
+
+  const handleIconPackChange = useCallback(
+    (
+      option: SingleValue<{
+        value: IconPack;
+        label: string;
+      }>,
+      actionMeta: ActionMeta<{
+        value: IconPack;
+        label: string;
+      }>
+    ) => {
+      setConfig({
+        ...config,
+        options: {
+          ...config.options,
+          iconPack: option?.value || "seti-icons",
         },
       });
     },
@@ -37,42 +76,6 @@ export default function PersonalizationTab() {
       </h1>
       <section>
         <h4>Sidebar Placement</h4>
-        {/* <div className="flex rounded bg-gray-200 p-2 w-max">
-          <section className="px-2">
-            <div
-              className={`w-32 h-16 rounded border-2 flex flex-col items-center justify-center ${
-                config.options.sidebar === "left" ? "border-blue-400" : ""
-              }`}
-              onClick={() => {
-                setConfig({
-                  ...config,
-                  options: {
-                    ...config.options,
-                    sidebar: "left",
-                  },
-                });
-              }}
-            />
-            <h6 className="text-center font-roboto pt-1">Left</h6>
-          </section>
-          <section className="px-2">
-            <div
-              className={`w-32 h-16 rounded border-2 flex flex-col items-center justify-center ${
-                config.options.sidebar === "right" ? "border-blue-400" : ""
-              }`}
-              onClick={() => {
-                setConfig({
-                  ...config,
-                  options: {
-                    ...config.options,
-                    sidebar: "right",
-                  },
-                });
-              }}
-            />
-            <h6 className="text-center font-roboto pt-1">Right</h6>
-          </section>
-        </div> */}
         <section className="w-48">
           <Select
             options={[
@@ -91,42 +94,22 @@ export default function PersonalizationTab() {
       </section>
       <section className="mt-4">
         <h4>Icon Pack</h4>
-        <div className="flex rounded bg-gray-200 p-2 w-max">
-          <section className="px-2">
-            <div
-              className={`w-32 h-16 rounded border-2 flex flex-col items-center justify-center ${
-                config.options.sidebar === "left" ? "border-blue-400" : ""
-              }`}
-              onClick={() => {
-                setConfig({
-                  ...config,
-                  options: {
-                    ...config.options,
-                    sidebar: "left",
-                  },
-                });
-              }}
-            />
-            <h6 className="text-center font-roboto pt-1">Left</h6>
-          </section>
-          <section className="px-2">
-            <div
-              className={`w-32 h-16 rounded border-2 flex flex-col items-center justify-center ${
-                config.options.sidebar === "right" ? "border-blue-400" : ""
-              }`}
-              onClick={() => {
-                setConfig({
-                  ...config,
-                  options: {
-                    ...config.options,
-                    sidebar: "right",
-                  },
-                });
-              }}
-            />
-            <h6 className="text-center font-roboto pt-1">Right</h6>
-          </section>
-        </div>
+        <section className="w-48">
+          <Select
+            options={[
+              { value: "seti-icons", label: "Seti Icons" },
+              { value: "vscode-icons", label: "VSCode Icons" },
+              { value: "material-icons", label: "Material Icons" },
+            ]}
+            defaultValue={{
+              value: config.options.iconPack,
+              label:
+                config.options.iconPack.charAt(0).toUpperCase() +
+                config.options.iconPack.slice(1),
+            }}
+            onChange={handleIconPackChange}
+          />
+        </section>
       </section>
     </div>
   );
