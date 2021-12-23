bd4cbe872b2a26897d655c52bf6af2fbf660c5f2
DeprecatedLuxas <lucasmathias65@icloud.com>
2021-12-23 13:24:38 +0100

    breadcrumb more dynamic

--- src/components/VSCode/Breadcrumbs.tsx
+++ src/components/VSCode/Breadcrumbs.tsx
@@ -1,22 +1,25 @@
 import Element from "./Element";
 import { VscChevronRight } from "react-icons/vsc";
-import { TypeScriptReactIcon } from "./icons";
+import { useRecoilValue } from "recoil";
+import { vscodeState } from "@recoil/atoms/vscode";
+
 export default function Breadcrumbs() {
+  const vscode = useRecoilValue(vscodeState);
+  const path = vscode.activeFile.path.split("/");
+
   return (
     <Element
       className="h-5.5 px-3 flex items-center leading-5.5"
       bind={["bg@breadcrumb.background", "c@breadcrumb.foreground"]}
     >
-      <Element>src</Element>
-
-      <VscChevronRight />
-      <Element>pages</Element>
-
-      <VscChevronRight />
-
-      <Element>
-        index.tsx
-      </Element>
+      {path.map((item, index) => (
+        <>
+          {/* TODO: ICON COMPONENT */}
+          {index === path.length - 1 && <p>ICONCOMPONENT{vscode.activeFile.iconPath}</p>}
+          <Element>{item}</Element>
+          {index !== path.length - 1 && <VscChevronRight />}
+        </>
+      ))}
     </Element>
   );
 }
