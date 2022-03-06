dae8c90d5f521d9fea9c1cf414f00be31381bc6d
DeprecatedLuxas <lucasnrgaard@gmail.com>
2022-03-06 17:25:21 +0100

    mappings package

---
+++ packages/mappings/src/index.ts
@@ -0,0 +1,3 @@
+export { default as fileMappings } from "../json/file-mappings.json";
+export { default as folderMappings } from "../json/folder-mappings.json";
+export { default as languageMappings } from "../json/language-mappings.json";
