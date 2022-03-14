04d09930b8cdf7a2a06041e014d01ce2f87a39d3
DeprecatedLuxas <lucasmathias65@icloud.com>
2022-03-14 09:55:34 +0100

    removed old core device detection

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,7 +2,6 @@ export const DEV = process.env.NODE_ENV !== "production";
 
 export * from "./color";
 export * from "./dom";
-export * from "./detection";
 export * from "./marketplace";
 export * from "./webbie"; // Browser Storage Wrapper
 export * from "./featon"; // Feature Detection Library
