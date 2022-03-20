88ebf8cb6b4b85de279016dde4cf2c4e20838ec6
DeprecatedLuxas <lucasnrgaard@gmail.com>
2022-03-20 22:02:37 +0100

    removed keybinds for now

--- apps/web/src/lib/enums/index.ts
+++ apps/web/src/lib/enums/index.ts
@@ -1,5 +1,2 @@
 export { default as MessageStatusEnum } from "./MessageStatus";
 export { default as MessagePositionEnum } from "./MessagePosition";
-export { default as ModifierKeysEnum } from "./ModifierKeys";
-export { default as KeyCodesEnum } from "./KeyCodes";
-export { default as KeyNamesEnum } from "./KeyNames";
