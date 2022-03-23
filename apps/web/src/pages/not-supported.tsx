70d13b0f56199ff5c57ad0eb630730cb995f654a
DeprecatedLuxas <lucasnrgaard@gmail.com>
2022-03-23 22:20:30 +0100

    missing design on not-supported page

--- apps/web/src/pages/not-supported.tsx
+++ apps/web/src/pages/not-supported.tsx
@@ -1,10 +1,19 @@
-import { isBrowser } from "@theme-studio/core";
+import { Button } from "@components/Forms";
+import Link from "next/link";
 
 export default function NotSupportedPage() {
   return (
-    <div>
-      <h1>Device is not supported</h1>
-      <p>{JSON.stringify(isBrowser())}</p>
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="bg-green-700">
+        <h1 className="">Your device was not supported</h1>
+        <p>
+          We are sorry your device wasn&apos;t supported. Please try again on
+          your computer.
+        </p>
+        <Link href="/" passHref>
+          <a className="">Go Back</a>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
