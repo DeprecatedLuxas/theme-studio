08785910e97a71a57012ea298fd8d6a4df0e1c23
DeprecatedLuxas <lucasnrgaard@gmail.com>
2022-03-28 08:53:46 +0200

    more cleanup

--- packages/ui/src/components/configuration/index.tsx
+++ packages/ui/src/components/configuration/index.tsx
@@ -1,15 +1,5 @@
-import { ReactNode } from "react";
-import { ConfigurationProvider } from "../../providers/configuration-provider";
-
 export {
   ConfigurationSidebar,
   type ConfigurationSidebarProps,
 } from "./ConfigurationSidebar";
-
-export interface ConfigurationProps {
-  children: ReactNode;
-}
-
-export function Configuration({ children }: ConfigurationProps) {
-  return <ConfigurationProvider>{children}</ConfigurationProvider>;
-}
+export { Configuration, type ConfigurationProps } from "./Configuration";
