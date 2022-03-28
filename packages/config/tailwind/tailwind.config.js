5f4059516c1c10371e0cc97f971dada3d961fe26
DeprecatedLuxas <lucasnrgaard@gmail.com>
2022-03-28 10:34:37 +0200

    Update tailwind.config.js

--- packages/config/tailwind/tailwind.config.js
+++ packages/config/tailwind/tailwind.config.js
@@ -12,24 +12,19 @@ module.exports = {
     backgroundImage: {
       grid: 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAMElEQVQ4T2N89uzZfwY8QFJSEp80A+OoAcMiDP7//483HTx//hx/Ohg1gIFx6IcBALl+VXknOCvFAAAAAElFTkSuQmCC")',
     },
-    colors: {
-      transparent: "transparent",
-      white: "#fff",
-      black: "#000",
-      gray: {
-        900: "#202225",
-        800: "#2f3136",
-        700: "#36393f",
-        600: "#4f545c",
-        400: "#d4d7dc",
-        300: "#e3e5e8",
-        200: "#ebedef",
-        100: "#f2f3f5",
-      },
-      blue: colors.blue,
-      red: colors.red,
-    },
     extend: {
+      colors: {
+        gray: {
+          900: "#202225",
+          800: "#2f3136",
+          700: "#36393f",
+          600: "#4f545c",
+          400: "#d4d7dc",
+          300: "#e3e5e8",
+          200: "#ebedef",
+          100: "#f2f3f5",
+        },
+      },
       spacing: {
         1.875: "1.875rem",
         5.5: "1.375rem",
